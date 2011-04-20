472e4b32603eef61e12e2f16253cd519462693e3
Wesley McClure <wmcclure@phoenixwebgroup.com>
2011-04-20 15:16:55 -0500

    Days forwards/backwards streams

---
+++ src/BclExtensionMethodTests/DaysTests.cs
@@ -0,0 +1,35 @@
+﻿namespace BclExtensionMethodTests
+{
+	using System;
+	using System.Linq;
+	using BclExtensionMethods;
+	using NUnit.Framework;
+
+	[TestFixture]
+	public class DaysTests : AssertionHelper
+	{
+		[Test]
+		public void BackwardsFrom_TakeTwo_ReturnsTwoDates()
+		{
+			var startDate = new DateTime(2010, 1, 2);
+			var previousDate = startDate.AddDays(-1);
+
+			var twoDaysBackwards = Days.BackwardsFrom(startDate)
+				.Take(2);
+
+			Expect(twoDaysBackwards, Is.EquivalentTo(new[] { startDate, previousDate }));
+		}
+		
+		[Test]
+		public void ForwardsFrom_TakeTwo_ReturnsTwoDates()
+		{
+			var startDate = new DateTime(2010, 1, 2);
+			var nextDate = startDate.AddDays(1);
+
+			var twoDaysBackwards = Days.ForwardsFrom(startDate)
+				.Take(2);
+
+			Expect(twoDaysBackwards, Is.EquivalentTo(new[] { startDate, nextDate }));
+		}
+	}
+}
---
+++ src/BclExtensionMethods/Days.cs
@@ -0,0 +1,42 @@
+﻿namespace BclExtensionMethods
+{
+	using System;
+	using System.Collections.Generic;
+
+	public class Days
+	{
+		public static DateTime Tomorrow
+		{
+			get { return DateTime.Today.AddDays(1); }
+		}
+
+		public static DateTime Yesterday
+		{
+			get { return DateTime.Today.AddDays(-1); }
+		}
+
+		/// <summary>
+		/// 	Returns an infinite day stream backwards (inclusive) from the startDate
+		/// </summary>
+		public static IEnumerable<DateTime> BackwardsFrom(DateTime startDate)
+		{
+			while (true)
+			{
+				yield return startDate;
+				startDate = startDate.AddDays(-1);
+			}
+		}
+
+		/// <summary>
+		/// 	Returns an infinite day stream forwards (inclusive) from the startDate
+		/// </summary>
+		public static IEnumerable<DateTime> ForwardsFrom(DateTime startDate)
+		{
+			while (true)
+			{
+				yield return startDate;
+				startDate = startDate.AddDays(1);
+			}
+		}
+	}
+}
