645a48c9c4b128b67ceb20f1420a9676dcfab888
Wesley McClure <wmcclure@phoenixwebgroup.com>
2011-05-02 12:55:45 -0500

    Decimal shift operation

---
+++ src/BclExtensionMethodTests/NumericExtensionTests.cs
@@ -0,0 +1,30 @@
+﻿namespace BclExtensionMethodTests
+{
+	using BclExtensionMethods;
+	using NUnit.Framework;
+
+	public class NumericExtensionTests : AssertionHelper
+	{
+		[Test]
+		public void ShiftDecimalBy_TenShiftedOnePlace_IsOneHundred()
+		{
+			var ten = 10m;
+			var onePlace = 1;
+
+			var shifted = ten.ShiftDecimalBy(onePlace);
+
+			Expect(shifted, Is.EqualTo(100m));
+		}
+
+		[Test]
+		public void ShiftDecimalBy_TenShiftedByNegativeOnePlace_IsOne()
+		{
+			var ten = 10m;
+			var negativeOnePlace = -1;
+
+			var shifted = ten.ShiftDecimalBy(negativeOnePlace);
+
+			Expect(shifted, Is.EqualTo(1m));
+		}
+	}
+}
---
+++ src/BclExtensionMethods/NumericExtensions.cs
@@ -0,0 +1,21 @@
+﻿namespace BclExtensionMethods
+{
+	using System;
+
+	public static class NumericExtensions
+	{
+		/// <summary>
+		/// Decimal Shift Operation
+		/// </summary>
+		/// <param name="places">+ shifts decimal right, - shifts decimal left, this is done via Powers of 10</param>
+		/// <example>10.ShiftDecimalBy(1) == 100</example>
+		public static decimal ShiftDecimalBy(this decimal number, int places)
+		{
+			if (places == 0)
+			{
+				return number;
+			}
+			return number*(decimal) Math.Pow(10, places);
+		}
+	}
+}
