9105c9f890fc8dfcb3258909e18cfda900221e6a
Wesley McClure <wmcclure@phoenixwebgroup.com>
2011-05-02 13:19:39 -0500

    NullIf extensions

---
+++ src/BclExtensionMethods/ValueTypes/NullIfExtensions.cs
@@ -0,0 +1,21 @@
+﻿namespace BclExtensionMethods.ValueTypes
+{
+	public static class NullIfExtensions
+	{
+		public static T? NullIf<T>(this T? number, T value)
+			where T : struct
+		{
+			if (number != null && number.Equals(value))
+			{
+				return null;
+			}
+			return number;
+		}
+
+		public static T? NullIf<T>(this T number, T value)
+			where T : struct
+		{
+			return ((T?) number).NullIf(value);
+		}
+	}
+}
