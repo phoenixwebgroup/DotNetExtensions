5a82b2acfd8870e3a8b084b111ac1339d14b8129
Wesley McClure <wmcclure@phoenixwebgroup.com>
2011-05-26 15:51:51 -0500

    Initial nuget rake task for DotNetExtensions

--- DotNetExtensions/Rakefile.rb
+++ DotNetExtensions/Rakefile.rb
@@ -3,6 +3,8 @@ require 'albacore'
 
 $projectSolution = 'DotNetExtensions.sln'
 $artifactsPath = "build"
+$nugetPath = "nuget"
+$nugetFeedPath = "%PAC%/nuget"
 
 task :teamcity => [:build_release]
 
@@ -17,4 +19,24 @@ end
 task :clean do
     puts "Cleaning"
     FileUtils.rm_rf $artifactsPath
+end
+
+task :nuget => [:nuget_DotNetExtensions, :nuget_Financial]
+
+task :nuget_DotNetExtensions => [:build] do
+	source =  File.join($artifactsPath, 'BclExtensionMethods.*')
+	destination = File.join($nugetPath, 'DotNetExtensions-Base/lib/net40')
+	FileUtils.mkdir_p(destination) unless File.directory?(destination)
+	FileUtils.cp_r(Dir.glob(source), destination)
+	nugetFeedLocation = File.join($nugetFeedPath, 'DotNetExtensions-Base')
+	sh "nuget pack " + File.join($nugetPath, 'DotNetExtensions-Base', 'DotNetExtensions-Base.nuspec') + " /OutputDirectory " + nugetFeedLocation
+end
+
+task :nuget_Financial => [:build] do
+	source =  File.join($artifactsPath, 'Financial.*')
+	destination = File.join($nugetPath, 'DotNetExtensions-Financial/lib/net40')
+	FileUtils.mkdir_p(destination) unless File.directory?(destination)
+	FileUtils.cp_r(Dir.glob(source), destination)
+	nugetFeedLocation = File.join($nugetFeedPath, 'DotNetExtensions-Financial')
+	sh "nuget pack " + File.join($nugetPath, 'DotNetExtensions-Financial', 'DotNetExtensions-Financial.nuspec') + " /OutputDirectory " + nugetFeedLocation
 end
