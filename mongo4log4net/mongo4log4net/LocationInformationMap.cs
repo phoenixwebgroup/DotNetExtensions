5b0e5afb7283fff058df1f426a0bc1940c4b320d
Wesley McClure <wmcclure@phoenixwebgroup.com>
2011-06-10 11:23:39 -0500

    Copy over mongo4log4net, setup nuget and rake builds

---
+++ mongo4log4net/mongo4log4net/LocationInformationMap.cs
@@ -0,0 +1,16 @@
+﻿namespace mongo4log4net
+{
+	using MongoDB.Bson.Serialization;
+	using log4net.Core;
+
+	public class LocationInformationMap : BsonClassMap<LocationInfo>
+	{
+		public LocationInformationMap()
+		{
+			MapProperty(c => c.ClassName);
+			MapProperty(c => c.LineNumber);
+			MapProperty(c => c.MethodName);
+			MapProperty(c => c.FileName);
+		}
+	}
+}
---
+++ mongo4log4net/mongo4log4net/MongoAppenderInit.cs
@@ -0,0 +1,25 @@
+﻿namespace mongo4log4net
+{
+	using System;
+	using MongoDB.Bson.Serialization;
+	using MongoDB.Bson.Serialization.Conventions;
+
+	public class MongoAppenderInit
+	{
+		public static Func<MongoAppenderInit> Provider { get; set; }
+
+		static MongoAppenderInit()
+		{
+			Provider = () => new MongoAppenderInit();
+		}
+
+		public virtual void Init()
+		{
+			var profile = new ConventionProfile();
+			profile.SetMemberFinderConvention(new LoggingMemberFinderConvention());
+			BsonClassMap.RegisterClassMap(new ExceptionMap());
+			BsonClassMap.RegisterClassMap(new LocationInformationMap());
+			BsonClassMap.RegisterConventions(profile, t => true);
+		}
+	}
+}
