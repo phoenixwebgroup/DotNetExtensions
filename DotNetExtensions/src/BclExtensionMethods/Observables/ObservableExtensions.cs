b6b60ae7c0b5dce10a617153f4863dcf2bfdb7f1
Wesley McClure <wmcclure@phoenixwebgroup.com>
2011-06-16 14:15:46 -0500

    Add throttle after first extension method to observables (instead of Throttle built in which returns the last one in a window.

---
+++ DotNetExtensions/src/BclExtensionMethods/Observables/ObservableExtensions.cs
@@ -0,0 +1,21 @@
+﻿namespace BclExtensionMethods.Observables
+{
+	using System;
+	using System.Reactive.Linq;
+
+	public static class ObservableExtensions
+	{
+		/// <summary>
+		/// Throttle a stream of events, only taking the first item after which the timespan will apply to squelch any subsequent events.
+		/// At time A, event T happens, any events from A to (A + delay) will be ignored, upon the next event after (A + delay) the same rule will apply to the first event T.
+		/// </summary>
+		public static IObservable<T> ThrottleAfterFirst<T>(this IObservable<T> source, TimeSpan delay)
+		{
+			return source
+				.Select(item => new { TimeStamp = DateTime.Now, Item = item })
+				.Scan((last, current) => current.TimeStamp.Subtract(last.TimeStamp) >= delay ? current : last)
+				.Select(s => s.Item)
+				.DistinctUntilChanged();
+		} 
+	}
+}
