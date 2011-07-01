27caf62d5415b9b415c32f4ba1ebb026fc9672f9
Wesley McClure <wmcclure@phoenixwebgroup.com>
2011-07-01 17:01:27 -0500

    ConcurrentCollection

---
+++ DotNetExtensions/src/BclExtensionMethods/Concurrency/ConcurrentCollection.cs
@@ -0,0 +1,50 @@
+﻿namespace BclExtensionMethods.Concurrency
+{
+	using System.Collections;
+	using System.Collections.Concurrent;
+	using System.Collections.Generic;
+
+	/// <summary>
+	/// 	Concurrent collection built on top of a concurrent dictionary.
+	/// </summary>
+	public class ConcurrentCollection<T> : IEnumerable<T>
+	{
+		private readonly ConcurrentDictionary<T, bool> _ConcurrentDictionary;
+
+		public ConcurrentCollection()
+		{
+			_ConcurrentDictionary = new ConcurrentDictionary<T, bool>();
+		}
+
+		public bool TryAdd(T item)
+		{
+			return _ConcurrentDictionary.TryAdd(item, true);
+		}
+
+		public bool TryRemove(T item)
+		{
+			bool value;
+			return _ConcurrentDictionary.TryRemove(item, out value);
+		}
+
+		public bool Contains(T item)
+		{
+			return _ConcurrentDictionary.ContainsKey(item);
+		}
+
+		public void Clear()
+		{
+			_ConcurrentDictionary.Clear();
+		}
+
+		public IEnumerator<T> GetEnumerator()
+		{
+			return _ConcurrentDictionary.Keys.GetEnumerator();
+		}
+
+		IEnumerator IEnumerable.GetEnumerator()
+		{
+			return GetEnumerator();
+		}
+	}
+}
