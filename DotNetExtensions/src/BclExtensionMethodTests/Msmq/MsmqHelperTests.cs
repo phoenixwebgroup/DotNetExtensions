b558079ce8c236afd0053ac430d9b1fdff561838
Wesley McClure <wes.mcclure@gmail.com>
2011-10-21 19:06:30 -0500

    Msmq helper methods, IsQueueEmpty

---
+++ DotNetExtensions/src/BclExtensionMethodTests/Msmq/MsmqHelperTests.cs
@@ -0,0 +1,68 @@
+﻿namespace BclExtensionMethodTests.Msmq
+{
+	using System.Messaging;
+	using BclExtensionMethods.Msmq;
+	using NUnit.Framework;
+
+	[TestFixture]
+	public class MsmqHelperTests : AssertionHelper
+	{
+		private string _QueueName;
+
+		[SetUp]
+		public void Setup()
+		{
+			_QueueName = @".\private$\TestQueue-FB4E2BFA-1AC0-4B59-9ABD-A5938625BC6A";
+			SetupNewQueue(_QueueName);
+		}
+
+		[TearDown]
+		public void Teardown()
+		{
+			DeleteQueueIfItExists(_QueueName);
+		}
+
+		[Test]
+		public void IsQueueEmpty_EmptyQueue_ReturnsTrue()
+		{
+			var isEmpty = MsmqHelper.IsQueueEmpty(_QueueName);
+
+			Expect(isEmpty);
+		}
+
+		[Test]
+		public void IsQueueEmpty_NotEmptyQueue_ReturnsFalse()
+		{
+			var message = new Message();
+			var queue = new MessageQueue(_QueueName);
+			queue.Send(message);
+
+			var isEmpty = MsmqHelper.IsQueueEmpty(_QueueName);
+
+			Expect(isEmpty, Is.False);
+		}
+
+		[Test]
+		[ExpectedException(typeof (MessageQueueException))]
+		public void IsQueueEmpty_InvalidQueue_ThrowsException()
+		{
+			var invalidQueueName = @".\private$\invalidQueueName";
+
+			MsmqHelper.IsQueueEmpty(invalidQueueName);
+		}
+
+		private void SetupNewQueue(string queueName)
+		{
+			DeleteQueueIfItExists(queueName);
+			MessageQueue.Create(queueName);
+		}
+
+		private static void DeleteQueueIfItExists(string queueName)
+		{
+			if (MessageQueue.Exists(queueName))
+			{
+				MessageQueue.Delete(queueName);
+			}
+		}
+	}
+}
---
+++ DotNetExtensions/src/BclExtensionMethods/Msmq/MsmqHelper.cs
@@ -0,0 +1,29 @@
+﻿namespace BclExtensionMethods.Msmq
+{
+	using System;
+	using System.Messaging;
+
+	public static class MsmqHelper
+	{
+		/// <summary>
+		/// 	Checks for one second to ensure the queue is empty
+		/// </summary>
+		public static bool IsQueueEmpty(string queuePath)
+		{
+			try
+			{
+				var queue = new MessageQueue(queuePath, QueueAccessMode.Peek);
+				queue.Peek(TimeSpan.FromSeconds(1));
+			}
+			catch (MessageQueueException exception)
+			{
+				if (exception.MessageQueueErrorCode == MessageQueueErrorCode.IOTimeout)
+				{
+					return true;
+				}
+				throw;
+			}
+			return false;
+		}
+	}
+}
