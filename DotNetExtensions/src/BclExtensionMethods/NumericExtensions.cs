b3f72668985d2bad2ee7bc5d02931fc53cfc91c2
Wesley McClure <wes.mcclure@gmail.com>
2011-10-21 19:20:51 -0500

    Is even/odd numeric extension method

--- DotNetExtensions/src/BclExtensionMethods/NumericExtensions.cs
+++ DotNetExtensions/src/BclExtensionMethods/NumericExtensions.cs
@@ -22,5 +22,15 @@
 		{
 			return number % multiple == 0;
 		}
+
+		public static bool IsEven(this int value)
+		{
+			return value % 2 == 0;
+		}
+
+		public static bool IsOdd(this int value)
+		{
+			return value % 2 == 1;
+		}
 	}
 }
