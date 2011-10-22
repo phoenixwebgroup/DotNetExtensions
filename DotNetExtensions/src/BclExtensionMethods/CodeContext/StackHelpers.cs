9b33be39066eec024d00fd9024b6a9f37820f326
Wesley McClure <wes.mcclure@gmail.com>
2011-10-21 19:13:09 -0500

    Stack helper to get method name currently running, helpful for testing

---
+++ DotNetExtensions/src/BclExtensionMethods/CodeContext/StackHelpers.cs
@@ -0,0 +1,14 @@
+﻿namespace BclExtensionMethods.CodeContext
+{
+	using System.Diagnostics;
+	using System.Linq;
+	using System.Reflection;
+
+	public class StackHelpers
+	{
+		public static MethodBase GetMethodThatCalledMe()
+		{
+			return new StackTrace().GetFrames().Skip(2).First().GetMethod();
+		}
+	}
+}
