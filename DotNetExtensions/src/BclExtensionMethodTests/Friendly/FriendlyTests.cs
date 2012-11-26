e9837364e581c4283c23e23569ade466b62aa784
Wesley McClure <wes.mcclure@gmail.com>
2012-11-26 14:58:05 -0600

    Add tests of the code we got from the internets

--- DotNetExtensions/src/BclExtensionMethodTests/Friendly/FriendlyTests.cs
+++ DotNetExtensions/src/BclExtensionMethodTests/Friendly/FriendlyTests.cs
@@ -59,5 +59,70 @@
 
 			Expect(friendly, Is.EqualTo("5 minutes ago"));
 		}
+
+		[Test]
+		public void TimeSince_WithinLastMinute_ReturnsJustNow()
+		{
+			Expect(TimeSpan.Parse("00:00:00").TimeSince(), Is.EqualTo("just now"));
+			Expect(TimeSpan.Parse("00:00:59").TimeSince(), Is.EqualTo("just now"));
+		}
+
+		[Test]
+		public void TimeSince_BetweenOneAndTwoMinutes_ReturnsOneMinuteAgo()
+		{
+			Expect(TimeSpan.Parse("00:01:00").TimeSince(), Is.EqualTo("1 minute ago"));
+			Expect(TimeSpan.Parse("00:01:59").TimeSince(), Is.EqualTo("1 minute ago"));
+		}
+
+		[Test]
+		public void TimeSince_BetweenTwoMinutesAndOneHour_ReturnsMinutesRoundedDown()
+		{
+			Expect(TimeSpan.Parse("00:02:00").TimeSince(), Is.EqualTo("2 minutes ago"));
+			Expect(TimeSpan.Parse("00:59:59").TimeSince(), Is.EqualTo("59 minutes ago"));
+		}
+
+		[Test]
+		public void TimeSince_BetweenOneHourAndTwoHours_ReturnsOneHourAgo()
+		{
+			Expect(TimeSpan.Parse("01:00:00").TimeSince(), Is.EqualTo("1 hour ago"));
+			Expect(TimeSpan.Parse("01:59:59").TimeSince(), Is.EqualTo("1 hour ago"));
+		}
+
+		[Test]
+		public void TimeSince_BetweenTwoHoursAndOneDay_ReturnsHours()
+		{
+			Expect(TimeSpan.Parse("02:00:00").TimeSince(), Is.EqualTo("2 hours ago"));
+			Expect(TimeSpan.Parse("23:59:59").TimeSince(), Is.EqualTo("23 hours ago"));
+		}
+
+		[Test]
+		public void TimeSince_BetweenOneDayAndTwoDays_ReturnsYesterday()
+		{
+			Expect(TimeSpan.FromDays(1).TimeSince(), Is.EqualTo("yesterday"));
+			Expect(TimeSpan.FromDays(2).Subtract(TimeSpan.FromSeconds(1)).TimeSince(), Is.EqualTo("yesterday"));
+		}
+
+		[Test]
+		public void TimeSince_BetweenTwoDaysAndOneWeek_ReturnsDays()
+		{
+			Expect(TimeSpan.FromDays(2).TimeSince(), Is.EqualTo("2 days ago"));
+			Expect(TimeSpan.FromDays(7).Subtract(TimeSpan.FromSeconds(1)).TimeSince(), Is.EqualTo("6 days ago"));
+		}
+
+		[Test]
+		public void TimeSince_GreaterThanOneWeekButLessThan31Days_ReturnsWeeks()
+		{
+			Expect(TimeSpan.FromDays(7).TimeSince(), Is.EqualTo("1 weeks ago"));
+			Expect(TimeSpan.FromDays(14).TimeSince(), Is.EqualTo("2 weeks ago"));
+			Expect(TimeSpan.FromDays(21).TimeSince(), Is.EqualTo("3 weeks ago"));
+			Expect(TimeSpan.FromDays(28).TimeSince(), Is.EqualTo("4 weeks ago"));
+			Expect(TimeSpan.FromDays(30).TimeSince(), Is.EqualTo("5 weeks ago"));
+		}
+
+		[Test]
+		public void TimeSince_GreaterThanOneMonth_ReturnsNull()
+		{
+			Expect(TimeSpan.FromDays(32).TimeSince(), Is.Null);
+		}
 	}
 }
